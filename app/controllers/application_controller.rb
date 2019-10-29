2f3bfffa9159b0365dee32518c2a76a66de9074d
Tarun Gupta <gr8tarun96@gmail.com>
2019-10-29 23:38:07 +0530

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-	#With this configuration, we’re now ready log users in.
 	protect_from_forgery with: :exception
   	include SessionsHelper
 end
